ad242c8793fd2a70338d498020d8fd77af646a5f
Khensani Ngobeni <khensi.ngobeni@gmail.com>
2023-11-22 18:55:07 -0800

    8. DBStorage - Place

--- console.py
+++ console.py
@@ -84,7 +84,7 @@ class HBNBCommand(cmd.Cmd):
         except Exception as mess:
             pass
         finally:
-            return line
+                return line
 
     def postcmd(self, stop, line):
         """Prints if isatty is false"""
@@ -114,7 +114,7 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, args):
-    """ Create an object of any class with given parameters"""
+        """ Create an object of any class with given parameters"""
     if not args:
         print("** class name missing **")
         return
